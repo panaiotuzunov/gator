753b98656268897e3e39318a0cefac1f572a572d
panaiotuzunov <panaiot.uzunov@gmail.com>
2025-07-11 18:03:18 +0300

    implement login

--- command.go
+++ command.go
@@ -16,20 +16,28 @@ type command struct {
 }
 
 type commands struct {
-	command map[string]func(*state, command) error
+	list map[string]func(*state, command) error
 }
 
 func (c *commands) run(s *state, cmd command) error {
+	function, ok := c.list[cmd.name]
+	if !ok {
+		return fmt.Errorf("error: function does not exist")
+	}
+	err := function(s, cmd)
+	if err != nil {
+		return fmt.Errorf("%v", err)
+	}
 	return nil
 }
 
 func (c *commands) register(name string, f func(*state, command) error) {
-
+	c.list[name] = f
 }
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) != 1 {
-		return fmt.Errorf("error: The login handler accepts exactly one argument - username")
+		return fmt.Errorf("error: the login handler accepts exactly one argument - username")
 	}
 	err := s.cfg.SetUser(cmd.args[0])
 	if err != nil {
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/panaiotuzunov/gator/internal/config"
 )
@@ -9,17 +10,22 @@ import (
 func main() {
 	configStruct, err := config.Read()
 	if err != nil {
-		fmt.Println("oouuppsss")
+		fmt.Printf("cound not read config file - %v\n", err)
+		os.Exit(1)
 	}
-	err = configStruct.SetUser("panaiotuzunov")
+	stateStruct := state{cfg: &configStruct}
+	cmds := &commands{
+		list: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+	if len(os.Args) < 2 {
+		fmt.Println("error: not enough arguments")
+		os.Exit(1)
+	}
+	cmd := command{name: os.Args[1], args: os.Args[2:]}
+	err = cmds.run(&stateStruct, cmd)
 	if err != nil {
-		fmt.Println("oouuppsss")
-	} else {
-		fmt.Println("Success!")
+		fmt.Printf("running command %v failed with %v\n", cmd.name, err)
+		os.Exit(1)
 	}
-	configStruct, err = config.Read()
-	if err != nil {
-		fmt.Println("oouuppsss")
-	}
-	fmt.Printf("%+v\n", configStruct)
 }
