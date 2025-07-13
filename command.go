1d01d67d5e5de4e7dd76fa6f94280f7482618810
Panaiot Uzunov <panaiotuzunov@gmail.com>
2025-07-13 22:03:58 +0300

    add implement register users, sql connection

--- command.go
+++ command.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"os"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/panaiotuzunov/gator/internal/config"
+	"github.com/panaiotuzunov/gator/internal/database"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -39,10 +46,50 @@ func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) != 1 {
 		return fmt.Errorf("error: the login handler accepts exactly one argument - username")
 	}
-	err := s.cfg.SetUser(cmd.args[0])
-	if err != nil {
-		return err
+	usernameStr := cmd.args[0]
+	_, err := s.db.GetUserByName(context.Background(), sql.NullString{String: usernameStr, Valid: true})
+	if err == sql.ErrNoRows {
+		fmt.Printf("User %s does not exist.\n", usernameStr)
+		os.Exit(1)
+	} else if err != nil {
+		return fmt.Errorf("error: database error - %v", err)
 	}
-	fmt.Println("The user was set successfully.")
+	err = s.cfg.SetUser(usernameStr)
+	if err != nil {
+		return fmt.Errorf("error updating config: %v", err)
+	}
+	fmt.Printf("The user %s logged in successfully.\n", usernameStr)
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("error: the register handler accepts exactly one argument - username")
+	}
+	usernameStr := cmd.args[0]
+	_, err := s.db.GetUserByName(context.Background(), sql.NullString{String: usernameStr, Valid: true})
+	if err == sql.ErrNoRows {
+		userData := database.CreateUserParams{
+			ID:        uuid.New(),
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+			Name:      sql.NullString{String: usernameStr, Valid: true},
+		}
+		CreatedUserData, err := s.db.CreateUser(context.Background(), userData)
+		if err != nil {
+			fmt.Printf("Error creating user %s.\n", usernameStr)
+			os.Exit(1)
+		}
+		err = s.cfg.SetUser(usernameStr)
+		if err != nil {
+			return fmt.Errorf("error updating config: %v", err)
+		}
+		fmt.Printf("User %s created successfully. User parameters:\n%v", usernameStr, CreatedUserData)
+	} else if err != nil {
+		return fmt.Errorf("error: database error - %v", err)
+	} else {
+		fmt.Printf("User %s already exists.\n", usernameStr)
+		os.Exit(1)
+	}
 	return nil
 }
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
 
+	_ "github.com/lib/pq"
 	"github.com/panaiotuzunov/gator/internal/config"
+	"github.com/panaiotuzunov/gator/internal/database"
 )
 
 func main() {
@@ -17,7 +20,17 @@ func main() {
 	cmds := &commands{
 		list: make(map[string]func(*state, command) error),
 	}
+	db, err := sql.Open("postgres", configStruct.DbUrl)
+	if err != nil {
+		fmt.Printf("Could connect to SQL DB - %v\n", err)
+	}
+	stateStruct.db = database.New(db)
 	cmds.register("login", handlerLogin)
+	if len(os.Args) < 2 {
+		fmt.Println("error: not enough arguments")
+		os.Exit(1)
+	}
+	cmds.register("register", handlerRegister)
 	if len(os.Args) < 2 {
 		fmt.Println("error: not enough arguments")
 		os.Exit(1)
